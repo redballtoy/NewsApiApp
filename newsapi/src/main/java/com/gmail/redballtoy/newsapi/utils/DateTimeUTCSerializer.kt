1915f396c154dda7eb907b754b4d289b2d2fa890
Dmitry Sorokin <redballtoy@gmail.com>
2024-03-18 11:35:24 +0300

    refactoding2

--- newsapi/src/main/java/com/gmail/redballtoy/newsapi/utils/DateTimeUTCSerializer.kt
+++ newsapi/src/main/java/com/gmail/redballtoy/newsapi/utils/DateTimeUTCSerializer.kt
@@ -10,7 +10,9 @@ import java.util.Date
 
 internal object DateTimeUTCSerializer : KSerializer<Date> {
     override val descriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
+
     override fun serialize(encoder: Encoder, value: Date) = encoder.encodeString(value.toString())
+
     override fun deserialize(decoder: Decoder): Date =
         DateFormat.getDateTimeInstance().parse(decoder.decodeString())
 }
