717e30f6b24da725ed3a1c6b0cc5a2b0aa3367cf
Dmitry Sorokin <redballtoy@gmail.com>
2024-03-18 14:05:43 +0300

    refactor DTO

--- newsapi/src/main/java/com/gmail/redballtoy/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/gmail/redballtoy/newsapi/NewsApi.kt
@@ -1,9 +1,9 @@
 package com.gmail.redballtoy.newsapi
 
 import androidx.annotation.IntRange
-import com.gmail.redballtoy.newsapi.models.Article
+import com.gmail.redballtoy.newsapi.models.ArticleDTO
 import com.gmail.redballtoy.newsapi.models.Languages
-import com.gmail.redballtoy.newsapi.models.Response
+import com.gmail.redballtoy.newsapi.models.ResponseDTO
 import com.gmail.redballtoy.newsapi.models.SortBy
 import com.gmail.redballtoy.newsapi.utils.NewsApiKeyInterceptor
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
@@ -38,7 +38,7 @@ interface NewsApi {
         @Query("sortBy") sortBy: SortBy? = null,
         @Query("pageSize") @IntRange(from = 0, to = 100) pageSize: Int = 100,
         @Query("page") @IntRange(from = 1) page: Int = 1,
-    ): Result<Response<Article>>
+    ): Result<ResponseDTO<ArticleDTO>>
 }
 
 fun newApi(
--- newsapi/src/main/java/com/gmail/redballtoy/newsapi/models/ResponseDTO.kt
+++ newsapi/src/main/java/com/gmail/redballtoy/newsapi/models/ResponseDTO.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Response<E>(
+data class ResponseDTO<E>(
     @SerialName("status") val status: String,
     @SerialName("totalResult") val totalResult:Int,
     @SerialName("articles") val articles :List<E>
