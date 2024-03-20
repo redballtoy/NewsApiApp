b3401b1da3ce7315d0574a7a1cdace1ec107454a
Dmitry Sorokin <redballtoy@gmail.com>
2024-03-20 10:39:31 +0300

    create database

--- database/src/main/java/com/gmail/redballtoy/database/NewsDatabase.kt
+++ database/src/main/java/com/gmail/redballtoy/database/NewsDatabase.kt
@@ -1,4 +1,22 @@
 package com.gmail.redballtoy.database
 
-class NewsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.gmail.redballtoy.database.dao.ArticleDao
+import com.gmail.redballtoy.database.models.ArticleDBO
+
+@Database(entities = [ArticleDBO::class], version = 1)
+abstract class NewsDatabase : RoomDatabase() {
+
+    abstract fun getArticlesDao(): ArticleDao
+
+}
+
+fun NewsDatabase(applicationContext: Context): NewsDatabase {
+    return Room.databaseBuilder(
+        checkNotNull(applicationContext.applicationContext),
+        NewsDatabase::class.java, "news"
+    ).build()
 }
---
+++ database/src/main/java/com/gmail/redballtoy/database/dao/ArticleDao.kt
@@ -0,0 +1,24 @@
+package com.gmail.redballtoy.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.gmail.redballtoy.database.models.ArticleDBO
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ArticleDao {
+
+    @Query("SELECT * FROM articles")
+    suspend fun getAll(): Flow<List<ArticleDBO>>
+
+    @Insert
+    suspend fun insert(articles: List<ArticleDBO>)
+
+    @Delete
+    suspend fun remove(articles: List<ArticleDBO>)
+
+    @Query("DELETE FROM articles")
+    suspend fun clear()
+}
