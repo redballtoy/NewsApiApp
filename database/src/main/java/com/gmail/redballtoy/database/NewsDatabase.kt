50b3278c6a817617b36d739e7502369bb9cbbe1a
Dmitry Sorokin <redballtoy@gmail.com>
2024-03-20 11:07:04 +0300

    add concerters for data

--- database/src/main/java/com/gmail/redballtoy/database/NewsDatabase.kt
+++ database/src/main/java/com/gmail/redballtoy/database/NewsDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.gmail.redballtoy.database.dao.ArticleDao
 import com.gmail.redballtoy.database.models.ArticleDBO
+import com.gmail.redballtoy.database.utils.Converters
 
 @Database(entities = [ArticleDBO::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class NewsDatabase : RoomDatabase() {
 
     abstract fun getArticlesDao(): ArticleDao
--- database/src/main/java/com/gmail/redballtoy/database/dao/ArticleDao.kt
+++ database/src/main/java/com/gmail/redballtoy/database/dao/ArticleDao.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.Flow
 interface ArticleDao {
 
     @Query("SELECT * FROM articles")
-    suspend fun getAll(): Flow<List<ArticleDBO>>
+    fun getAll(): Flow<List<ArticleDBO>>
 
     @Insert
     suspend fun insert(articles: List<ArticleDBO>)
