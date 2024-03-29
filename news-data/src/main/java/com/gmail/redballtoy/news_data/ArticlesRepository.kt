93aa953b51b12324805f551a8e251fb7b78a08cc
Dmitry Sorokin <redballtoy@gmail.com>
2024-03-29 15:29:34 +0300

    refactor Repository 4

--- news-data/src/main/java/com/gmail/redballtoy/news_data/ArticlesRepository.kt
+++ news-data/src/main/java/com/gmail/redballtoy/news_data/ArticlesRepository.kt
@@ -8,70 +8,64 @@ import com.gmail.redballtoy.newsapi.models.ArticleDTO
 import com.gmail.redballtoy.newsapi.models.ResponseDTO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
 
 
 class ArticlesRepository(
-    private val database: NewsDatabase,
-    private val api: NewsApi
+    private val database: NewsDatabase, private val api: NewsApi
 ) {
     fun getAll(): Flow<RequestResult<List<Article>>> {
 
         //local cache
         val cachedAllArticles = getAllFromDatabase()
+            .map { result ->
+                result.map { articlesDbos ->
+                    articlesDbos?.map { it.toArticle() }
+                }
+            }.map { it.map { } }
 
         //remote data
         val remoteArticles: Flow<RequestResult<*>> = getAllFromServer()
 
+        //merge result used with order
+        return cachedAllArticles.combine(remoteArticles) { dbos: RequestResult<Article>, dtos: RequestResult<Article> ->
 
-        //merge result
-        cachedAllArticles.map
-        {
-
-        }
-
-        return cachedAllArticles.combine(remoteArticles)
-        {
 
         }
-
     }
 
-    private fun getAllFromServer(): Flow<RequestResult<*>> {
-        return flow {
-            emit(api.everything())
-        }.map { result ->
-            if (result.isSuccess) {
-                val response: ResponseDTO<ArticleDTO> = result.getOrThrow()
-                RequestResult.Success(response.articles)
-            } else {
-                RequestResult.Error(null)
-            }
-        }.onEach { requestResult ->
-            if (requestResult is RequestResult.Success) {
-                requestResult.requireData()
-                    .map {
-                        articleDTO -> articleDTO.toArticleDbo()
-                    }
+    private fun getAllFromServer(): Flow<RequestResult<ResponseDTO<ArticleDTO>>> {
 
-                requestResult.map { dtos ->
-                    dtos.map { articleDTO ->
-                        articleDTO.toArticleDbo()
-                    }
-                }
-                database.articleDao.insert(requestResult.data)
+        val apiRequest = flow {
+            emit(api.everything())
+        }.onEach { result ->
+            if (result.isSuccess) {
+                saveNetResponseToCache(checkNotNull(result.getOrThrow().articles))
             }
+        }.map {
+            it.toRequestResult()
         }
 
+        //emit inProgress
+        val start = flowOf<RequestResult<ResponseDTO<ArticleDTO>>>(RequestResult.InProgress())
+
+        //union flows
+        return merge(apiRequest, start)
+    }
+
+    private suspend fun saveNetResponseToCache(data: List<ArticleDTO>) {
+        val dbos = data.map { articleDTO ->
+            articleDTO.toArticleDbo()
+        }
+        database.articleDao.insert(dbos)
     }
 
     private fun getAllFromDatabase(): Flow<RequestResult.Success<List<ArticleDBO>>> {
-        return database.articleDao
-            .getAll()
-            .map { RequestResult.Success(it) }
+        return database.articleDao.getAll().map { RequestResult.Success(it) }
     }
 
     suspend fun search(query: String): Flow<Article> {
@@ -80,23 +74,31 @@ class ArticlesRepository(
     }
 }
 
-sealed class RequestResult<E>(internal val data: E) {
-    class InProgress<E>(data: E) : RequestResult<E>(data)
+sealed class RequestResult<E>(internal val data: E? = null) {
+    class InProgress<E>(data: E? = null) : RequestResult<E>(data)
     class Success<E>(data: E) : RequestResult<E>(data)
-    class Error<E>(data: E) : RequestResult<E>(data)
+    class Error<E> : RequestResult<E>()
 }
 
 
-internal fun <T : Any> RequestResult<T?>.requireData(): T {
-    return checkNotNull(data)
-}
+internal fun <T : Any> RequestResult<T?>.requireData(): T = checkNotNull(data)
+
 
 //custom mapping
-internal fun <In, Out> RequestResult<In>.map(mapper: (In) -> Out): RequestResult<Out> {
+internal fun <In, Out> RequestResult<In>.map(mapper: (In?) -> Out): RequestResult<Out> {
     val outData = mapper(data)
     return when (this) {
         is RequestResult.Success -> RequestResult.Success(outData)
-        is RequestResult.Error -> RequestResult.Error(outData)
+        is RequestResult.Error -> RequestResult.Error()
         is RequestResult.InProgress -> RequestResult.InProgress(outData)
+    }
+}
+
+//convert Result(kotlin) to ResultRequest
+internal fun <T> Result<T>.toRequestResult(): RequestResult<T> {
+    return when {
+        isSuccess -> RequestResult.Success(getOrThrow())
+        isFailure -> RequestResult.Error()
+        else -> error("Impossible branch")
     }
 }
--- news-data/src/main/java/com/gmail/redballtoy/news_data/Mappers.kt
+++ news-data/src/main/java/com/gmail/redballtoy/news_data/Mappers.kt
@@ -8,6 +8,10 @@ internal fun ArticleDBO.toArticle():Article{
     TODO("Not Implement")
 }
 
+internal fun ArticleDTO.toArticle():Article{
+    TODO("Not Implement")
+}
+
 internal fun ArticleDTO.toArticleDbo(): ArticleDBO {
     TODO("Not yet implemented")
 }
