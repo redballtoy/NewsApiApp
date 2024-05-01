34402674ae5ac33cc67406cf59a886cbf809c563
Dmitry Sorokin <redballtoy@gmail.com>
2024-05-01 11:09:51 +0300

    Add hilt and kapt

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,7 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.kapt) apply false
 }
 
--- news-data/build.gradle.kts
+++ news-data/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.kapt)
 }
 
 android {
@@ -36,8 +38,8 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.kotlinx.koroutines.android)
-
-
     implementation(project(":database"))
     implementation(project(":newsapi"))
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.compiler)
 }
